6b5d8ff4e5d28bde82579db0faff98ed3644fdf5
Christyan Simbolon <sambungdata@gmail.com>
2025-11-18 23:47:39 +0700

    fix: DB url to PostgrSQL

--- services/llm_services/app/db/session.py
+++ services/llm_services/app/db/session.py
@@ -6,8 +6,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+psycopg2://postgres:postgres@localhost:5432/cortex_llm")
-
+# DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+psycopg2://postgres:postgres@localhost:5432/cortex_llm")
+DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
